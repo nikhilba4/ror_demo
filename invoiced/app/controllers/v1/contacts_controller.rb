70e43705c81d05ff90c8358f07d7321c8d2918eb
nikhilbang44 <nikhilbang44@gmail.com>
2018-04-16 17:33:26 +0530

    Setting up Basic CRUD

---
+++ invoiced/app/controllers/v1/contacts_controller.rb
@@ -0,0 +1,27 @@
+class V1::ContactsController < ApplicationController
+	def index
+		@contacts = Contact.all
+		render json: @contacts, status: :ok
+	end
+
+	def create
+		@contact = Contact.new(contact_params)
+		@contact.save
+		render json: @contact, status: :created
+	end
+
+	def destory
+		@contact = Contact.where(:id params[:id]).first
+		if @contact.destory
+			head(:ok)
+		else
+			head(:unprocessable_entity)
+		end
+	end
+
+	private
+
+	def contact_params
+		params.require(:contact).permit(:first_name, :last_name, :email)	
+	end
+end
